d38bb23c814dbb6d62ef4131c7b1572ff7cb93d5
随心 <1023293003@qq.com>
2017-09-19 10:26:42 +0800

    备份

---
+++ oa-persist/src/main/java/z_tknight/oa/persist/complex/mapper/AuthorizationMapper.java
@@ -0,0 +1,12 @@
+package z_tknight.oa.persist.complex.mapper;
+
+/**
+ * 授权认证持久层接口
+ * 
+ * @author 随心
+ *
+ */
+public interface AuthorizationMapper {
+
+	
+}
--- oa-service/src/main/java/z_tknight/oa/service/baseService/CardService.java
+++ oa-service/src/main/java/z_tknight/oa/service/baseService/CardService.java
@@ -4,9 +4,12 @@ import z_tknight.oa.commons.util.ResponeResult;
 
 public interface CardService {
 
+	public ResponeResult selectCardInfoByCardNo(Integer userNo, Integer cardNo);
 	public ResponeResult addCard(String cardName, Integer userNo, Integer listNo);
 	public ResponeResult deleteCard(Integer cardNo, Integer userNo);
-	public ResponeResult updateCardName(Integer userNo, String newCardName, Integer cardNo);
-	public ResponeResult updateCardOrder(Integer cardNo, Integer listNoFrom, Integer listNoTo, String cardOrderFrom, String cardOrderTo, 
+	public ResponeResult updateCardInfo(Integer userNo, String newCardName, 
+			Double workLoad, Long endTime, Integer cardNo);
+	public ResponeResult updateCardOrder(Integer cardNo, Integer listNoFrom, 
+			Integer listNoTo, String cardOrderFrom, String cardOrderTo, 
 			String newcardOrderFrom, String newcardOrderTo, Integer userNo);
 }
